713ccc0906d747bd2134a39c7cd11729de8f54f4
alessandrizes <alessandrizes@gmail.com>
2020-10-17 14:31:11 -0300

    update: exercicio-01

--- exercicios-aula/exercicio-01/README.md
+++ exercicios-aula/exercicio-01/README.md
@@ -2,4 +2,3 @@
 01. Fazer a chamada usando fetch para pegar a imagem.
 02. Colocar a imagem na tag img no body.
 03. Lidar com situções de erro.
-04. Mudar a chamada de fetch para async await.
--- exercicios-aula/exercicio-01/script.js
+++ exercicios-aula/exercicio-01/script.js
@@ -2,15 +2,34 @@
 //02. Colocar a imagem na tag img no body.
 const img = document.getElementById("rainbow")
 
-function clicar() {
-    fetch("rainbow.jpg") // Busca imagem
-        .then(function(response) { // então espera o retorno da promisse
-            return response.blob() // retorna o objeto blob(), fornece um objeto do tipo arquivo
-        })
-        .then(function(blob) {
-            const url = URL.createObjectURL(blob) // criamos uma URL de objeto blob
-            img.src =  url // inserimos a url criada na tag img
-        })
-}
+// function clicar() {
+//     fetch("rainbows.jpg") // Busca imagem
+//         .then(function(response) { // então espera o retorno da promisse
+//             return response.blob() // convertendo o objeto blob
+//         })
+//         .then(function(blob) {
+//             const url = URL.createObjectURL(blob) // criamos uma URL de objeto blob
+//             img.src =  url // inserimos a url criada na tag img
+//         })
+// }
 
 // 03. Lidar com situções de erro.
+function clicar() {
+    fetch("rainbows.jpg")
+        .then(function(response) {
+            console.log(response)
+            // console.log(response.ok)
+            // console.log(response.status)
+            if(response.ok){
+                response.blob().then(function(objeto){
+                    console.log(objeto)
+                    img.src = URL.createObjectURL(objeto)
+                })
+            } else {
+                alert("Olha, eu não achei essa img!")
+            }
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
+}
